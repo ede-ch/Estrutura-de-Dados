d999859295866c4ec7474bdd62b8b2320460f771
Edelin Chaves <edelinchaves.dev@gmail.com>
2023-05-24 19:34:50 -0300

    TAD1

--- src/TAD1/App.java
+++ src/TAD1/App.java
@@ -1,5 +1,19 @@
 package TAD1;
 
 public class App {
+/*
+Implemente o TAD PontoCartesiano em Java
+Crie um método construtor;
+Getters e Setters;
+Crie um método que calcule a distância euclidiana entre dois pontos;
+Teste esse TAD em uma classe Main. */
+
+    public static void main(String[] args) {
+
+        PontoCartesiano p1 = new PontoCartesiano(1, 2);
+        PontoCartesiano p2 = new PontoCartesiano(4, 6);
+        PlanoCartesiano plano = new PlanoCartesiano(p1, p2);
+        System.out.println("Distância euclidiana: " + plano.distanciaEuclidiana());
+    }
     
 }
---
+++ src/TAD1/PlanoCartesiano.java
@@ -0,0 +1,43 @@
+package TAD1;
+
+public class PlanoCartesiano {
+
+    // atributos
+    private PontoCartesiano p1;
+    private PontoCartesiano p2;
+
+    // construtor
+    public PlanoCartesiano(PontoCartesiano p1, PontoCartesiano p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    // métodos getters e setters
+    public PontoCartesiano getP1() {
+        return p1;
+    }
+
+    public PontoCartesiano getP2() {
+        return p2;
+    }
+
+    public void setP1(PontoCartesiano p1) {
+        this.p1 = p1;
+    }
+
+    public void setP2(PontoCartesiano p2) {
+        this.p2 = p2;
+    }
+
+    // método que calcula a distância euclidiana entre dois pontos
+    public double distanciaEuclidiana() {
+        double x1 = p1.getX();
+        double y1 = p1.getY();
+        double x2 = p2.getX();
+        double y2 = p2.getY();
+
+        double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        return distancia;
+    }
+}
