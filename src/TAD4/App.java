8905c51db03a76c7b80aca19f7f8cc0ffb4778e6
Edelin Chaves <edelinchaves.dev@gmail.com>
2023-05-26 16:47:15 -0300

    Main do TAD4.

--- src/TAD4/App.java
+++ src/TAD4/App.java
@@ -7,4 +7,18 @@ public class App {
      * Implemente um método para fazer a impressão da lista.
      */
 
-}
+     ListaSimplesmenteEncadeada<Aluno> lista = new ListaSimplesmenteEncadeada<>();
+
+        Aluno aluno1 = new Aluno("João", 20);
+        Aluno aluno2 = new Aluno("Maria", 22);
+        Aluno aluno3 = new Aluno("Pedro", 19);
+
+        lista.inserir(aluno1);
+        lista.inserir(aluno2);
+        lista.inserir(aluno3);
+
+        lista.imprimir();
+    }
+
+
+
