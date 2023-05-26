20b5eaa630eb67a82c2567b8680ab94ac1b30536
Edelin Chaves <edelinchaves.dev@gmail.com>
2023-05-26 16:44:19 -0300

    TAD5

--- src/TAD5/App.java
+++ src/TAD5/App.java
@@ -1,5 +1,33 @@
 package TAD5;
 
 public class App {
-    
+
+    public static void main(String[] args) {
+        ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
+        
+        lista.inserir(10);
+        lista.inserir(20);
+        lista.inserir(30);
+        lista.inserir(40);
+
+        System.out.println("Impressão no sentido direto:");
+        lista.imprimirSentidoDireto();
+
+        System.out.println("Impressão no sentido reverso:");
+        lista.imprimirSentidoReverso();
+
+        int valorBusca = 20;
+        if (lista.buscar(valorBusca)) {
+            System.out.println("Valor " + valorBusca + " encontrado na lista.");
+        } else {
+            System.out.println("Valor " + valorBusca + " não encontrado na lista.");
+        }
+
+        int valorRemocao = 30;
+        lista.remover(valorRemocao);
+
+        System.out.println("Lista após remoção do valor " + valorRemocao + ":");
+        lista.imprimirSentidoDireto();
+    }
 }
+
