f766c0c630417c9765241ecdcffa8f137da2a15e
Edelin Chaves <edelinchaves.dev@gmail.com>
2023-05-26 16:40:38 -0300

    TAD4

--- src/TAD4/App.java
+++ src/TAD4/App.java
@@ -1,5 +1,10 @@
 package TAD4;
 
 public class App {
-    
+    /*
+     * Implemente um TAD LSE genérica de modo que seja possível inserir Alunos ou
+     * quaisquer outros tipos de objetos.
+     * Implemente um método para fazer a impressão da lista.
+     */
+
 }
